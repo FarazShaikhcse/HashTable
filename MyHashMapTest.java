1321c19794efef7f3a012331258e4fea778fff3a
Faraz Shaikh <farazshabbir@113AMBBLR-Faraz-Shabbir-Shaikh.local>
2021-09-15 21:55:07 +0530

    word frequency

---
+++ MyHashMapTest.java
@@ -0,0 +1,28 @@
+package main;
+
+public class MyHashMapTest {
+	
+	public static void findFrequency() {
+		String sentance = "To be or not to be";
+		MyHashMap<String,Integer> myHashMap = new MyHashMap<>();
+		String[] words = sentance.toLowerCase().split(" ");
+		for(String word : words) {
+			Integer value = myHashMap.get(word);
+			if(value == null) 
+				value = 1;
+			else 
+				value = value + 1;
+			myHashMap.add(word,value);
+		}
+		
+		System.out.println(myHashMap);
+
+	}
+	
+	public static void main(String[] args) {
+		findFrequency();
+		
+	}
+
+}
+
